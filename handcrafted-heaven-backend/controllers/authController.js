030444d7e024c1f8f2a17717b2c6d81bb82590d4
Michael Mwanza <mwanza.n.m@gmail.com>
2025-08-08 22:51:09 +0200

    auth error fix

--- handcrafted-heaven-backend/controllers/authController.js
+++ handcrafted-heaven-backend/controllers/authController.js
@@ -48,12 +48,10 @@ exports.register = async (req, res) => {
       },
     });
 
-    const isProd = process.env.NODE_ENV === "production";
-
     res.cookie("token", token, {
       httpOnly: true,
-      secure: isProd,
-      sameSite: isProd ? "none" : "lax",
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
